034c02ed47c282a124458e76618141249f6570cb
cristianlara-ss <javierlaraflores@gmail.com>
2023-07-12 22:08:03 -0400

    commit de hola

--- venv/app.py
+++ venv/app.py
@@ -10,13 +10,13 @@ CORS(app)
 def home():
     return 'Funciona?'
 
-@app.route("/predecir", methods=["POST"])
-def predecir():
-    json=request.get_json(force=True)
-    medidas=json['Medidas']
-    clf=joblib.load('modelo_entrenado2.pkl')
-    prediccion=clf.predict(medidas)
-    return 'Los datos que proporcionaste corresponde al porcentaje de {0}'.format(prediccion)
+#@app.route("/predecir", methods=["POST"])
+#def predecir():
+#    json=request.get_json(force=True)
+#    medidas=json['Medidas']
+#    clf=joblib.load('modelo_entrenado2.pkl')
+#    prediccion=clf.predict(medidas)
+#    return 'Los datos que proporcionaste corresponde al porcentaje de {0}'.format(prediccion)
 
 if __name__ == '__main__':
     app.run()
