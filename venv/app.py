d1d239242fbfbc0f5554ac5b95aee5024a3bce04
cristianlara-ss <javierlaraflores@gmail.com>
2023-07-12 21:07:30 -0400

    primer commit github

---
+++ venv/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import joblib
+import sklearn
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return 'Funciona?'
+
+@app.route("/predecir", methods=["POST"])
+def predecir():
+    json=request.get_json(force=True)
+    medidas=json['Medidas']
+    clf=joblib.load('modelo_entrenado2.pkl')
+    prediccion=clf.predict(medidas)
+    return 'Los datos que proporcionaste corresponde al porcentaje de {0}'.format(prediccion)
+
+if __name__ == '__main__':
+    app.run()
